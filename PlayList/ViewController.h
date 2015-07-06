030dd6f92a584e1cb6739e9194b3fa4cd5f3d1d9
Mariya <mbeketova@rambler.ru>
2015-07-06 17:22:48 +0300

    Initial Commit

---
+++ PlayList/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PlayList
+//
+//  Created by Admin on 06.07.15.
+//  Copyright (c) 2015 Mariya Beketova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PlayList/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PlayList
+//
+//  Created by Admin on 06.07.15.
+//  Copyright (c) 2015 Mariya Beketova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
